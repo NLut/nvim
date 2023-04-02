49d80df2a054cd02cb73337a290b091b0afa54c0
jjustmoon <natapat2001@gmail.com>
2023-04-02 12:56:35 +0700

    change 2 space tap to 4 space and swap file is avaiable

--- lua/natapat/core/set.lua
+++ lua/natapat/core/set.lua
@@ -5,12 +5,12 @@ opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 --no swapfile
-opt.swapfile = false
+opt.swapfile = true
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
@@ -29,7 +29,7 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- backspace
+-- backspac
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
