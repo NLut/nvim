45fcdebca770d5163cfaef0b7c9132d9904c7f17
JJustmoon <natapat2001@gmail.com>
2023-12-26 13:52:06 +0700

    new keybind for :Prettier

--- lua/natapat/core/remap.lua
+++ lua/natapat/core/remap.lua
@@ -35,3 +35,4 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/the
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>");
+vim.keymap.set("n", "<leader>pt", ":Prettier<CR>")
