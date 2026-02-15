cfe152cc45f1dbb897d89c1b8c47150b6e5cc051
NLut <Natapat2001@gmail.com>
2026-02-15 17:19:04 +0700

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+sed -i '2d' ~/.local/share/nvim/site/pack/packer/start/playground/plugin/nvim-treesitter-playground.lua
